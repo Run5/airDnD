f2c89fcbcd980961d506631ee758d2fc41857879
Chase Brashear <chase.brashear@gmail.com>
2021-06-17 09:14:20 -0700

    add error handling

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,13 @@
 // backend/routes/index.js
+// External Imports
 const express = require('express');
 const router = express.Router();
+
+// Internal Imports
+const apiRouter = require('./api');
+
+// Use the api routes
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
