a087ef3a9af7226ea1458de84fbb90e80432aa78
Chase Brashear <chase.brashear@gmail.com>
2021-06-17 09:50:59 -0700

    add login functionality and user auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,55 @@
 // backend/routes/api/index.js
+// External Imports
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+// Internal Imports
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+
+/* CODE FOR TESTING */
+
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// // GET /api/restore-user
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // GET /api/require-auth
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
 module.exports = router;
